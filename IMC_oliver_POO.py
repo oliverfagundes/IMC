05b91b55d95100bb9b7901d7bb7cb67e52cfd5ba
oliverfagundes <oliverfapere@gmail.com>
2025-08-15 00:26:40 -0300

    Ta chegando lá

--- IMC_oliver_POO.py
+++ IMC_oliver_POO.py
@@ -11,14 +11,14 @@ Created on Aug 14 2025
 # IMC = peso (em quilos) ÷ altura*2 (em metros)
 
 class Pessoa():
-    def __innit__(self, peso, altura, idade):
-        peso = float(input('Informe seu peso em kg: '))
-        altura = float(input("Informe a sua altura em metros: "))
-        idade = int(input("Informe a sua idade: "))
+    def __init__(self, peso, altura, idade):
+        self.peso = peso
+        self.altura = altura
+        self.idade = idade
 
-    def imc(self, peso, altura):
-        IMC = peso / (altura**2)
-        print(f"\Seu IMC é {IMC}")
+    def imc(self):
+        IMC = self.peso / (self.altura)**2
+        print(f"Seu IMC é {IMC}")
         return IMC
 
 # Acima de 20 anos
@@ -30,7 +30,7 @@ class Pessoa():
 # Acima de 40 - Obesidade Grau 3
 
     def acima_20(self, imc, idade):
-        if idade >= 20:
+        if self.idade >= 20:
             if imc < 18.5:
                 print('Abaixo do peso ideal')
             elif imc > 18.5 and imc < 25:
@@ -51,72 +51,72 @@ class Pessoa():
 
 # Entre 10 e 20 anos: 
     def entre10e20(self, idade, imc, genero):  
-        if idade < 20 and idade > 10 : 
-            if genero == 'F':
-                if idade <= 10:
+        if self.idade < 20 and self.idade > 10 : 
+            if self.genero() == 'F':
+                if self.idade <= 10:
                     if imc < 14.23:
                         print('Baixo peso')
                     elif imc >= 14.23 and imc < 20.19:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 11:
-                    if imc < 14.60:
+                elif self.idade == 11:
+                    if self.imc < 14.60:
                         print('Baixo peso')
                     elif imc >= 14.60 and imc < 21.18:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 12:
+                elif self.idade == 12:
                     if imc < 14.98:
                         print('Baixo peso')
                     elif imc >= 14.98 and imc < 22.17:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 13:
+                elif self.idade == 13:
                     if imc < 15.36:
                         print('Baixo peso')
                     elif imc >= 15.36 and imc < 23.08:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 14:
+                elif self.idade == 14:
                     if imc < 15.67:
                         print('Baixo peso')
                     elif imc >= 15.67 and imc < 23.88:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 15:
+                elif self.idade == 15:
                     if imc < 16.01:
                         print('Baixo peso')
                     elif imc >= 16.01 and imc < 24.29:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 16:
+                elif self.idade == 16:
                     if imc < 16.37:
                         print('Baixo peso')
                     elif imc >= 16.37 and imc < 24.74:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 17:
+                elif self.idade == 17:
                     if imc < 16.59:
                         print('Baixo peso')
                     elif imc >= 16.59 and imc < 25.23:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 18:
+                elif self.idade == 18:
                     if imc < 16.71:
                         print('Baixo peso')
                     elif imc >= 16.71 and imc < 25.56:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 19:
+                elif self.idade == 19:
                     if imc < 16.87:
                         print('Baixo peso')
                     elif imc >= 16.87 and imc < 25.85:
@@ -124,76 +124,91 @@ class Pessoa():
                     else:
                         print("Sobrepeso")
 
-            elif genero == 'M':
-                if idade <= 10:
+            elif self.genero() == 'M':
+                if self.idade <= 10:
                     if imc < 14.42:
                         print('Baixo peso')
                     elif imc >= 14.42 and imc < 19.60:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 11:
+                elif self.idade == 11:
                     if imc < 14.83:
                         print('Baixo peso')
                     elif imc >= 14.83 and imc < 20.35:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 12:
+                elif self.idade == 12:
                     if imc < 15.24:
                         print('Baixo peso')
                     elif imc >= 15.24 and imc < 21.12:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 13:
-                    if imc < 15.73:
+                elif self.idade == 13:
+                    if self.imc < 15.73:
                         print('Baixo peso')
                     elif imc >= 15.73 and imc < 21.93:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 14:
+                elif self.idade == 14:
                     if imc < 16.18:
                         print('Baixo peso')
                     elif imc >= 16.18 and imc < 22.27:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 15:
+                elif self.idade == 15:
                     if imc < 16.59:
                         print('Baixo peso')
                     elif imc >= 16.59 and imc < 23.63:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 16:
+                elif self.idade == 16:
                     if imc < 17.01:
                         print('Baixo peso')
                     elif imc >= 17.01 and imc < 24.45:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 17:
+                elif self.idade == 17:
                     if imc < 17.31:
                         print('Baixo peso')
                     elif imc >= 17.31 and imc < 25.28:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 18:
+                elif self.idade == 18:
                     if imc < 17.54:
                         print('Baixo peso')
                     elif imc >= 17.54 and imc < 25.95:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-                elif idade == 19:
+                elif self.idade == 19:
                     if imc < 17.80:
                         print('Baixo peso')
                     elif imc >= 17.80 and imc < 26.36:
                         print('Peso adequado')
                     else:
                         print("Sobrepeso")
-            else:
-                print('Infelizmente, ainda não há informações cinetíficas para o IMC de adolescentes não bináries.\ Espero que em breve possamos ter melhores infromações.')
+            elif self.genero() == "NB":
+                print('Infelizmente, ainda não há informações cinetíficas para o IMC de adolescentes não bináries.\nEspero que em breve possamos ter melhores infromações.')
+
+
+peso = float(input('Informe seu peso em kg: '))
+altura = float(input("Informe a sua altura em metros: "))
+idade = int(input("Informe a sua idade: "))
+
+pessoa1 = Pessoa(peso, altura, idade)
+imc_pessoa1 = pessoa1.imc
+print(f"Seu IMC é {imc_pessoa1}")
+if idade >= 20:
+    quadro = pessoa1.acima_20
+    print(quadro)
+else:
+    quadro = pessoa1.entre10e20
+    print(quadro)
