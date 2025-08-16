af604d8df521b3f9b3ef90906c0ef8967e897a59
oliverfagundes <oliverfapere@gmail.com>
2025-08-15 21:46:32 -0300

    Funciona e está curto

--- IMC_POO.py
+++ IMC_POO.py
@@ -50,28 +50,28 @@ class Pessoa:
         return self.sexo
 
 # Dicionário com idades: 
-dic = {'F': {10: [14.23, 20.19], 11: [14.60, 21.18], 12: [14.98, 22.17], 13: [15.36, 23.08], 14: [15.67, 23.88],
-        15: [16.01, 24.29], 16: [16.37, 24.74], 17: [16.59, 25.23], 18: [16.71, 25.56], 19: [16.87, 25.85]},
-        
-        'M': {10: [14.42, 19.60], 11: [14.83, 20.35], 12: [15.24, 21.12], 13: [15.73, 21.93], 14: [16.18, 22.27],
-        15: [16.59, 23.63], 16: [17.01, 24.45], 17: [17.31, 25.28], 18: [17.54, 25.95], 19: [17.80, 26.36]}}
+    dic = {'F': {10: [14.23, 20.19], 11: [14.60, 21.18], 12: [14.98, 22.17], 13: [15.36, 23.08], 14: [15.67, 23.88],
+            15: [16.01, 24.29], 16: [16.37, 24.74], 17: [16.59, 25.23], 18: [16.71, 25.56], 19: [16.87, 25.85]},
+            
+            'M': {10: [14.42, 19.60], 11: [14.83, 20.35], 12: [15.24, 21.12], 13: [15.73, 21.93], 14: [16.18, 22.27],
+            15: [16.59, 23.63], 16: [17.01, 24.45], 17: [17.31, 25.28], 18: [17.54, 25.95], 19: [17.80, 26.36]}}
 
 # Entre 10 e 20 anos: 
-def entre10e20(self, genero, imc):  
-    if 10 <= self.idade < 20 and genero in dic: 
-        limites = dic[genero].get(self.idade)
-        if limites:
-            baixo, normal = limites
-            if imc < baixo:
-                print("Baixo peso")
-            elif baixo <= imc < normal:
-                print("Peso adequado")
+    def entre10e20(self, genero, imc):  
+        if 10 <= self.idade < 20 and genero in self.dic: 
+            limites = self.dic[genero].get(self.idade)
+            if limites:
+                baixo, normal = limites
+                if imc < baixo:
+                    print("Baixo peso")
+                elif baixo <= imc < normal:
+                    print("Peso adequado")
+                else:
+                    print("Sobrepeso")
             else:
-                print("Sobrepeso")
-        else:
-            print("Idade não encontrada na tabela.")        
-    elif genero == "NB":
-        print('Infelizmente, ainda não há informações científicas para o IMC de adolescentes não bináries.\nEspero que em breve possamos ter melhores infromações.')
+                print("Idade não encontrada na tabela.")        
+        elif genero == "NB":
+            print('Infelizmente, ainda não há informações científicas para o IMC de adolescentes não bináries.\nEspero que em breve possamos ter melhores informações.')
 
 peso = float(input('Informe seu peso em kg: '))
 altura = float(input("Informe a sua altura em metros: "))
